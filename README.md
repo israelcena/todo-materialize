afce709e11b760af068c518b23051566bca30ae2
israelcena <israelsjm@gmail.com>
2023-10-10 05:14:58 -0300

    :tada: init Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Todo List
+
+The to-do list with card
+
+## Technologies
+
+* Vanila JavaScript
+* Materializecss
+
+## License
+
+* MIT
+
+## Author
+
+* Project by [@israelcena](https://github.com/israelcena)
---
+++ script.js
@@ -0,0 +1,9 @@
+// designed for IsraelCena
+// https://github.com/israelcena/todo-materialize
+
+
+// search bar
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, options);
+});
