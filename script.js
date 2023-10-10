5dbe00b37ccde6d9ab940c44d2878dc7828fdefc
israelcena <israelsjm@gmail.com>
2023-10-10 05:55:08 -0300

    :sparkles: feat: autocomplete options and functions

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // designed for IsraelCena
 // https://github.com/israelcena/todo-materialize
 
+const afterAutocomplete = () => {
+  console.log(instance)
+}
+
+const sortFunction = (a, b, inputString) => {
+  return a.indexOf(inputString) - b.indexOf(inputString);
+}
+
+const options = {
+  data: {
+    "apple":'https://placehold.it/250x250',
+  },
+  limit: 5, 
+  minLength: 2,
+  onAutocomplete: afterAutocomplete,
+  sortFunction
+}
 
 // search bar
 document.addEventListener('DOMContentLoaded', function() {
