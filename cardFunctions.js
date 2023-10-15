807da695225767da04237e8cdcf162bdcadd345e
israelcena <israelsjm@gmail.com>
2023-10-15 01:44:03 -0300

    :sparkles: feat: remove card

--- cardFunctions.js
+++ cardFunctions.js
@@ -1,11 +1,12 @@
 // Elements
 const cardContainerEl = document.getElementById('card-container')
 const formAddNewCardEl = document.getElementById('add-card-form')
+const removedEvent = new Event('removed')
 
 let cardComponent = (title = 'Novo Titulo', body = '', dateConclusion = '', image) => {
   return (
     `
-    <div class="col xl4 l6 m6 s12">
+    <div class="col xl4 l6 m6 s12 card-element">
       <div class="card medium sticky-action hoverable">
           <div class="card-image waves-effect waves-block waves-light">
             <img class="activator" src=${image}
@@ -24,7 +25,7 @@ let cardComponent = (title = 'Novo Titulo', body = '', dateConclusion = '', imag
               <i class="small material-icons">check</i>
             </a>
             <a href="#" class="right red-text" style="margin-right: 0;">
-              <i class="small material-icons">delete</i>
+              <i class="small material-icons delete">delete</i>
             </a>
           </div>
           <div class="card-reveal">
@@ -44,4 +45,20 @@ formAddNewCardEl.addEventListener('submit', (event) => {
   const tempImage = 'https://picsum.photos/550/300'
   cardContainerEl.innerHTML += cardComponent(title.value, body.value, dateConclusion.value, tempImage)
   event.target.reset()
+})
+
+cardContainerEl.addEventListener('click', (event) => {
+  const clickedItem = event.target
+  if (Array.from(clickedItem.classList).includes('delete')) {
+    clickedItem.closest('.card-element').remove()
+    cardContainerEl.dispatchEvent(removedEvent)
+  }
+})
+
+cardContainerEl.addEventListener('removed', () => {
+  M.toast({
+    html: 'Card Removido',
+    displayLength: '1500',
+    classes: 'green',
+  })
 })
