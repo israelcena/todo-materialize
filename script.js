682a4cfbcfdee05dde5087a3b6600c17dfc0b253
israelcena <israelsjm@gmail.com>
2023-10-14 22:46:38 -0300

    :sparkles:	feat: include modal and rename function

--- script.js
+++ script.js
@@ -11,18 +11,44 @@ const sortFunction = (a, b, inputString) => {
   return a.indexOf(inputString) - b.indexOf(inputString)
 }
 
-const options = {
+const autoCompleteOptions = {
   data: {
     'apple': 'https://placehold.it/250x250',
   },
-  limit: 5, 
+  limit: 5,
   minLength: 2,
-  onAutocomplete: afterAutocomplete,
-  sortFunction
+  onAutocomplete,
+  sortFunction,
 }
 
-// search bar
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.autocomplete');
-  var instances = M.Autocomplete.init(elems, options);
-});
+const modalOptions = {
+  opacity: 0.75,
+}
+
+const datePickerOptions = {
+  autoClose: true,
+  showMonthAfterYear: true,
+  showDaysInNextAndPreviousMonths: true,
+  format: 'dddd, d !de mmmm !de yyyy',
+  i18n: {
+    cancel: 'Cancelar',
+    clear: 'Limpar',
+    done: 'Fechar',
+    months: ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'],
+    monthsShort: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
+    weekdays: ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'],
+    weekdaysShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'],
+  }
+}
+
+// materialize events
+document.addEventListener('DOMContentLoaded', function () {
+  const inputAutoComplete = document.querySelectorAll('.autocomplete')
+  const topAutoComplete = M.Autocomplete.init(inputAutoComplete, autoCompleteOptions)
+
+  const modals = document.querySelectorAll('.modal')
+  const footerModal = M.Modal.init(modals, modalOptions)
+
+  const datePicker = document.querySelectorAll('.datepicker')
+  const modalDatePicker = M.Datepicker.init(datePicker, datePickerOptions)
+})
